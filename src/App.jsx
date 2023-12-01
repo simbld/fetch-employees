470214df06ce4eca7c305ba4e5c34ebc16c79f3e
sim <s_bullado51@hotmail.com>
2023-12-01 07:00:56 +0100

    Consume axios and useEffect for display first employe

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
 import DisplayEmployee from './components/DisplayEmployee';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-	const sampleEmployee = {
-		gender: 'male',
-		name: {
-			first: 'Charlie',
-			last: 'Thompson',
-		},
-		location: {
-			street: {
-				number: 761,
-				name: 'Tay Street',
-			},
-			city: 'Timaru',
-			postcode: 76111,
-		},
-		email: 'charlie.thompson@example.com',
-		picture: {
-			medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
-		},
+	const [employee, setEmployee] = useState(null);
+	const getEmployee = () => {
+		axios.get('https://randomuser.me/api?nat=en').then((response) => {
+			setEmployee(response.data.results[0]);
+		});
 	};
+	useEffect(() => {
+		getEmployee();
+	}, []);
 
 	return (
 		<>
-			<DisplayEmployee employee={sampleEmployee} />
+			<div className="App">
+				<DisplayEmployee employee={employee} />
+				<button type="button" onClick={getEmployee}>
+					Get employee
+				</button>
+			</div>
 		</>
 	);
 }
